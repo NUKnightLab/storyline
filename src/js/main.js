f5fda9b8c053235c8c5675332b2a13d6724b39bd
Divya Sasidharan <hello@shortdiv.com>
2017-01-18 15:46:26 -0600

    wip: draws a graph

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,11 @@ window.onload = function() {
   example.data().init().then(function(someVal) {
     Chart1 = new Chart.dimensions(500, 600, someVal, null);
     Canvas = new Chart.createCanvas(1000, 400, 'graph');
-    //Draw a single line//
-    var val1 = someVal[1];
-    var val2 = someVal[0];
-    thing = new Chart.createLine(val1[0], val1[1], val2[0], val2[1], 'red', 1);
-    Canvas.appendChild(thing)
+    for(var i=1; i < someVal.length; i++) {
+      var val2 = someVal[i];
+      var val1 = someVal[i-1];
+      thing = new Chart.createLine(val1[0], val1[1], val2[0], val2[1], 'red', 1);
+      Canvas.appendChild(thing)
+    }
   })
 }
