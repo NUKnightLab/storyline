c908fa0fc8a77628378ba3979821938ef651d0b3
Divya Sasidharan <hello@shortdiv.com>
2017-01-19 10:38:53 -0600

    fix: Increasing scale and adding outer/inner svg to handle viewports

--- src/js/chart.js
+++ src/js/chart.js
@@ -7,6 +7,11 @@ var Chart = {
   },
   createLine: function(x1, y1, x2, y2, color, w) {
     var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    //scale line
+    x1 = x1*10;
+    x2 = x2*10;
+    y1 = y1*10;
+    y2 = y2*10;
     line.setAttribute('x1', x1);
     line.setAttribute('y1', y1);
     line.setAttribute('x2', x2);
@@ -17,11 +22,19 @@ var Chart = {
   },
   createCanvas : function(w, h, containerId){
     var container = document.getElementById(containerId);
-    var canvas = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    canvas.setAttribute('width', w);
-    canvas.setAttribute('height', h);
-    container.appendChild( canvas );
-    return canvas;
+    var canvasOuter = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    var canvasInner = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    canvasOuter.setAttribute('width', w);
+    canvasOuter.setAttribute('height', h);
+    canvasOuter.setAttribute('viewbox', '0 0 800 600')
+    canvasOuter.setAttribute('class', 'outer')
+    canvasInner.setAttribute('width', w);
+    canvasInner.setAttribute('height', h);
+    canvasInner.setAttribute('viewbox', '0 0 800 600')
+    canvasInner.setAttribute('class', 'inner')
+    canvasOuter.append(canvasInner)
+    container.appendChild( canvasOuter );
+    return canvasOuter;
   }
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,13 @@ window.onload = function() {
   var Chart1, Canvas;
 
   example.data().init().then(function(someVal) {
-    Chart1 = new Chart.dimensions(500, 600, someVal, null);
-    Canvas = new Chart.createCanvas(1000, 400, 'graph');
+    Chart1 = new Chart.dimensions(1000, 1000, someVal, null);
+    Canvas = new Chart.createCanvas(5000, 1000, 'graph');
     for(var i=1; i < someVal.length; i++) {
       var val2 = someVal[i];
       var val1 = someVal[i-1];
       thing = new Chart.createLine(val1[0], val1[1], val2[0], val2[1], 'red', 1);
-      Canvas.appendChild(thing)
+      Canvas.childNodes[0].appendChild(thing)
     }
   })
 }
