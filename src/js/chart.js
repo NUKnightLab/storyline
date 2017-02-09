4080116513f940166f25246f24876c69658051f5
Divya Sasidharan <hello@shortdiv.com>
2017-02-09 17:51:27 -0600

    refactor: moving translate into sep func

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,10 +1,10 @@
-var Chart = function(dataObj, width) {
+var Chart = function(dataObj, width, height) {
     this.data = dataObj.data;
     this.bounds = dataObj.bounds;
     this.intervals = dataObj.intervals;
     this.markers = dataObj.markers;
     this.width = width;
-    this.height = (2/3) * width;
+    this.height = height || (2/3) * width;
     this.init();
 };
 
@@ -12,11 +12,11 @@ Chart.prototype = {
   init: function() {
     this.setRange();
     this.setScale();
-    this.elem = this.createCanvas();
+    this.setTranslation();
+    this.createCanvas();
     this.drawLine();
   },
   setRange: function() {
-    //assume bounds are moment objects//
     this.rangeX = Math.abs(this.bounds.maxX.valueOf() - this.bounds.minX.valueOf());
     this.rangeY = Math.abs(this.bounds.maxY - this.bounds.minY);
   },
@@ -26,41 +26,70 @@ Chart.prototype = {
     this.SCALEX = (this.width/this.rangeX);
     this.SCALEY = (this.height-AXIS_HEIGHT)/(this.rangeY);
   },
+  setTranslation: function() {
+    this.translateX = -1 * this.bounds.minX.valueOf() * this.SCALEX
+    this.translateY = -1 * (this.bounds.minY * this.SCALEY)
+  },
+  addTicks: function(axis, intervals) {
+    var range = `range${axis.toUpperCase()}`
+    return this[range]/intervals
+  },
   createTicks: function() {
+    var d3Format = require('d3-format');
+    var timeinUnix = {
+          "seconds": 1000,
+          "minutes": 6000,
+          "hours": 36000000,
+          "days": 86400000,
+          "months": 2592000000,
+          "years": 31622400000
+        }
+    var min = {};
+    for (var property in timeinUnix) {
+      var time = this.rangeX/timeinUnix[property]
+	    //set min to first property//
+      Object.keys(min).length === 0 ? min[property] = time : min
+
+      if(time < min[Object.keys(min)]) {
+	    min = {};
+        min[property] = time;
+      }
+    }
+
+    var timeTicks = Object.keys(min)[0]
     var totalTick = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
         g = document.createElementNS('http://www.w3.org/2000/svg', 'g'),
         data = this.data,
-        intervals = this.intervals,
-        endBound = this.bounds.maxX,
-        numTicks = Math.round(endBound/intervals.xTick),
-        TRANSLATEY= (this.bounds.maxY + Math.abs(this.bounds.minY)) * this.SCALEY,
+        intervals = Object.values(min)[0],
+        endBound = this.bounds.maxX.valueOf(),
+        numTicks = Math.round(endBound/intervals),
+        TRANSLATEY = 360 - 25,
         maxRange,
-        xLabels=[0];
+        xLabels=[],
+        year = this.bounds.minX.year();
    
-    maxRange = numTicks*intervals.xTick * this.SCALEX;
-    for(var j=0;j<numTicks;j++) {
-      var interval = j*intervals.xTick
-      xLabels.push(data[interval][0])
-    }
+    var spanOfData = (Math.round(min[timeTicks]) * this.SCALEX) + this.translateX;
+    var distBetweenTicks = Math.abs(this.width/spanOfData);
+    distBetweenTicks = this.width/min[timeTicks]
 
     g.setAttribute('fill', 'none')
 
-    for(var i=0; i<= numTicks; i++) {
+    for(var i=0; i<=min[timeTicks]; i++) {
       var tick = document.createElementNS('http://www.w3.org/2000/svg', 'g'),
           tickStroke = document.createElementNS('http://www.w3.org/2000/svg', 'line'),
           tickLabel = document.createElementNS('http://www.w3.org/2000/svg', 'text'),
           mark = i*intervals.xTick;
 
-      tickStroke.setAttribute('x2', 0);
+      tickStroke.setAttribute('x1', 16);
+      tickStroke.setAttribute('x2', 16);
       tickStroke.setAttribute('y2', 6);
       tickStroke.setAttribute('stroke', 'black');
-      tick.setAttribute("transform", `translate(${mark*this.SCALEX}, ${TRANSLATEY})`);
+      tick.setAttribute("transform", `translate(${distBetweenTicks*i}, ${TRANSLATEY})`);
       tickLabel.setAttribute("fill", "#000");
       tickLabel.setAttribute('x', 0.5);
       tickLabel.setAttribute('y', 9);
       tickLabel.setAttribute('dy', '0.71em');
-
-      tickLabel.innerHTML = (xLabels[i]);
+      tickLabel.innerHTML = (year + i);
       tick.append(tickStroke);
       tick.append(tickLabel);
       g.append(tick);
@@ -68,15 +97,13 @@ Chart.prototype = {
 
     var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute('stroke', '#000');
-    path.setAttribute('d', `M0,${TRANSLATEY}V${TRANSLATEY}H${maxRange}V${TRANSLATEY}`)
+    path.setAttribute('d', `M0,${TRANSLATEY}h${this.width}`)
     g.append(path)
-    totalTick.append(g);
-    return totalTick;
+    
+    return g;
   },
-  createMarkers: function(index, counter, translateX, translateY, rows) {
+  createMarkers: function(x, y, counter) {
     var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    var x = ((rows[0].valueOf() * this.SCALEX) + translateX);
-    var y = this.height - ((rows[1]*this.SCALEY) + translateY);
     circle.setAttribute('cx', x);
     circle.setAttribute('cy', y);
     circle.setAttribute('r', 5);
@@ -90,15 +117,13 @@ Chart.prototype = {
    * @returns {undefined}
    */
   drawLine: function() {
-   var translateX = -1 * this.bounds.minX.valueOf() * this.SCALEX,
-       translateY = -1 * (this.bounds.minY * this.SCALEY),
-       line = "",
+   var line = "",
        marks = [],
        counter = 0;
 
    for(var i=0; i < this.data.length; i++) {
-     var x = ((this.data[i][0].valueOf() * this.SCALEX) + translateX);
-     var y = this.height - (((this.data[i][1] * this.SCALEY)) + translateY);
+     var x = ((this.data[i][0].valueOf() * this.SCALEX) + this.translateX);
+     var y = this.height - (((this.data[i][1] * this.SCALEY)) + this.translateY);
 
      line += x + " " + y;
 
@@ -107,7 +132,7 @@ Chart.prototype = {
      }
 
      if(this.markers.indexOf(i) >=0) {
-       var mark = this.createMarkers(i, counter, translateX, translateY, this.data[i]);
+       var mark = this.createMarkers(x, y, counter);
        counter++;
        marks.push(mark);
        this.elem.appendChild(mark);
@@ -123,8 +148,8 @@ Chart.prototype = {
    lineEl.setAttribute('fill', 'none');
    this.elem.appendChild(lineEl);
 
-   //var tick = this.createTicks();
-   //this.elem.appendChild(tick);
+   var tick = this.createTicks();
+   this.elem.appendChild(tick);
   },
   /**
    * create an empty svg object "canvas" where line graph will be drawn
@@ -136,7 +161,7 @@ Chart.prototype = {
     canvasOuter.setAttribute('width', this.width);
     canvasOuter.setAttribute('height', this.height);
     canvasOuter.setAttribute('class', 'canvas')
-    return canvasOuter;
+    this.elem = canvasOuter;
   },
 }
 
--- test/chart-test.js
+++ test/chart-test.js
@@ -1,20 +1,61 @@
 import { Chart } from'../src/js/chart.js';
 import { expect, assert } from 'chai'
+import sinon from 'sinon'
+import moment from 'moment'
 
-describe('Create new graph', function() {
+describe('Checking chart functions', function() {
+  var Chart1
   describe('create a new graph', function() {
-    var Chart1;
+    var init;
     beforeEach(function() {
-      Chart1 = new Chart.dimensions(500, 600, null, null);
+      init = sinon.stub(Chart.prototype, 'init');
+      var data = [
+                   [moment("01/21/80", "MM/DD/YY"), 1],
+                   [moment("12/29/80", "MM/DD/YY"), 3],
+                   [moment("13/31/80", "MM/DD/YY"), 2],
+                   [moment("14/30/80", "MM/DD/YY"), 3],
+                   [moment("15/30/80", "MM/DD/YY"), 2],
+                  ]
+      Chart1 = new Chart(
+                 {
+                   data: data,
+                   bounds: {
+                     minX: null,
+                     maxX: null,
+                     minY: null,
+                     maxY: null
+                   },
+                   intervals: null,
+                   markers: null
+                 }, 500, 600)
     })
     it('should return the height of a new chart', function() {
-      expect(Chart1.h).to.eql(600);
+      expect(Chart1.height).to.eql(600);
     })
     it('should return the width of a new chart', function() {
-      expect(Chart1.w).to.eql(500);
+      expect(Chart1.width).to.eql(500);
     })
     afterEach(function() {
-      delete Chart1.dimensions;
+      init.restore();
+    })
+  })
+  describe('create a logical tick mark', function() {
+    var range;
+    beforeEach(function() {
+      range = sinon.stub(Chart1, 'setRange').returns(Chart1.rangeX = 50, Chart1.rangeY = 30)
+    })
+    it('should set the rangeX and rangeY of a chart', function() {
+      expect(Chart1.rangeX).to.eql(50)
+      expect(Chart1.rangeY).to.eql(30)
+    })
+    it('should return rangeX when given x as an input', function() {
+      expect(Chart1.addTicks('x')).to.eq(50)
+    })
+    it('should return rangeY when given y as an input', function() {
+      expect(Chart1.addTicks('y')).to.eq(30)
+    })
+    afterEach(function() {
+      range.restore();
     })
   })
 })
