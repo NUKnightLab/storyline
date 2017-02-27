5af62d334e6d4ba5a35e2dd058a3e17b5462608e
Divya Sasidharan <hello@shortdiv.com>
2017-02-27 14:26:13 -0600

    fix: Adding pubsub as a global and browser resize checker

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import {Storyline} from './storyline';
 
 window.Storyline = Storyline;
+
+/**
+ * onresize courtesy of this post: https://www.lullabot.com/articles/importing-css-breakpoints-into-javascript
+ *
+ * @returns {undefined}
+ */
+window.onresize = function() {
+  var bp = {}
+  bp.value = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+  PubSub.publish('window resized', bp)
+}
+
