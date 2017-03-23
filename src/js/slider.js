473b9dae52bba4d1ed91b3ba6b94ce03312b6488
Divya Sasidharan <hello@shortdiv.com>
2017-03-23 10:55:38 -0500

    fix: renaming variables for clarity

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,8 @@
 var Hammer = require('hammerjs');
 
-var Slider = function(slides, startIndex, height) {
+var Slider = function(cards, startIndex, height) {
   this.activeCard = startIndex;
-  this.slides = slides;
+  this.cards = cards;
   this.MARGIN = 10;
   this.height = height;
   this.createSlider();
@@ -13,13 +13,13 @@ Slider.prototype = {
   createSlider: function() {
     //create index key in slides for use in class naming by index in nav//
     //mustache should have something about iterating over index//
-    for(var i in this.slides) {
-      this.slides[i].index = i
+    for(var i in this.cards) {
+      this.cards[i].index = i
     }
-    this.cards = this.renderTemplate('slider-cards-template', this)
-    this.nav = this.renderTemplate('nav-template', this)
+    this.cardsElem = this.renderTemplate('slider-cards-template', this)
+    this.navElem = this.renderTemplate('nav-template', this)
     this.elem = this.createSliderView();
-    this.attachClickHandler(this.nav.children[0].children);
+    this.attachClickHandler(this.navElem.children[0].children);
   },
   /**
    * creates the slider view and appends slides to it
@@ -31,8 +31,8 @@ Slider.prototype = {
         sliderView.setAttribute('class', 'slider-view');
         sliderView.style.height = this.height + "px";
 
-    sliderView.appendChild(this.cards);
-    sliderView.appendChild(this.nav);
+    sliderView.appendChild(this.cardsElem);
+    sliderView.appendChild(this.navElem);
 
     return sliderView;
   },
@@ -68,23 +68,23 @@ Slider.prototype = {
   },
   setActiveCard: function(currentActiveCard, pastActiveCard) {
     this.activeCard = currentActiveCard;
-    if(this.cards.children[pastActiveCard].classList.contains('active')) {
-      this.cards.children[pastActiveCard].classList.remove('active');
-      this.nav.children[0].children[pastActiveCard].classList.remove('active');
+    if(this.cardsElem.children[pastActiveCard].classList.contains('active')) {
+      this.cardsElem.children[pastActiveCard].classList.remove('active');
+      this.navElem.children[0].children[pastActiveCard].classList.remove('active');
       storyline.chart.markers[pastActiveCard].classList.remove('active')
     }
-    this.cards.children[currentActiveCard].classList.add('active');
-    this.nav.children[0].children[currentActiveCard].classList.add('active');
+    this.cardsElem.children[currentActiveCard].classList.add('active');
+    this.navElem.children[0].children[currentActiveCard].classList.add('active');
     storyline.chart.markers[currentActiveCard].classList.add('active')
   },
   setTrayPosition: function(index, pastIndex) {
     index = index!=undefined ? index : this.activeCard;
     pastIndex = pastIndex | 0;
 
-    var card = this.cards.children[index];
+    var card = this.cardsElem.children[index];
     var move = ((card.offsetLeft/this.sliderWidth) * 100) - this.offsetPercent
     this.currentOffset = - move
-    this.cards.style.transform = 'translateX(' +  this.currentOffset  + "%)";
+    this.cardsElem.style.transform = 'translateX(' +  this.currentOffset  + "%)";
 
     this.setActiveCard(index, pastIndex)
   },
@@ -100,41 +100,45 @@ Slider.prototype = {
     } else {
       w = 500;
     }
-    var numSlides = this.slides.length;
-    this.viewportSize = this.cards.parentElement.clientWidth;
+    this.viewportSize = this.cardsElem.parentElement.clientWidth;
+    var offset = this.viewportSize/2 - w/2;
     this.cardWidth = w
-    this.sliderWidth = w * numSlides
-    this.offset = this.viewportSize/2 - w/2;
-    this.offsetPercent = this.offset/(w*numSlides) * 100
-    this.cards.style.width = this.sliderWidth + "px"
-    this.cards.style.transform = 'translateX(' + this.offsetPercent + '%)';
-    for(var i = 0; i < numSlides; i++) {
-      this.cards.children[i].style.width = w + "px";
-      this.cards.children[i].style.border = this.MARGIN + "px solid white";
+    this.sliderWidth = w * this.cards.length
+    this.offsetPercent = offset/(w*this.cards.length) * 100
+    this.cardsElem.style.width = this.sliderWidth + "px"
+    this.cardsElem.style.transform = 'translateX(' + this.offsetPercent + '%)';
+    for(var i = 0; i < this.cards.length; i++) {
+      this.cardsElem.children[i].style.width = w + "px";
+      this.cardsElem.children[i].style.border = this.MARGIN + "px solid white";
     }
   },
+  /**
+   * Calculates position of slider offset based on dragging the card
+   *
+   * @returns {undefined}
+   */
   slideCard: function() {
     var self = this;
     var offset;
+    var transformPercentage = 0;
     var percentage = 0;
-    var perc = 0;
     var goToSlide = function(number) {
       if(number < 0) {
         self.activeCard = 0;
-      } else if(number > self.slides.length - 1) {
+      } else if(number > self.cards.length - 1) {
         self.activeCard = number - 1
       } else {
         self.setActiveCard(number, self.activeCard)
         self.activeCard = number;
       }
 
-      self.cards.classList.add('is-animating')
-      var percentage = -(100 / self.slides.length) * self.activeCard;
+      self.cardsElem.classList.add('is-animating')
+      var percentage = -(100 / self.cards.length) * self.activeCard;
       percentage = percentage + self.offsetPercent
-      self.cards.style.transform = 'translateX(' + percentage + '%)';
+      self.cardsElem.style.transform = 'translateX(' + percentage + '%)';
       clearTimeout(timer);
       var timer = setTimeout(function() {
-        self.cards.classList.remove( 'is-animating' );
+        self.cardsElem.classList.remove( 'is-animating' );
       }, 400 );
     }
     var handleHammer = function(ev) {
@@ -142,24 +146,24 @@ Slider.prototype = {
       switch(ev.type) {
         case 'panleft':
         case 'panright':
-          perc = (ev.deltaX/self.sliderWidth) * 100
-          percentage = perc + self.currentOffset
+          percentage = (ev.deltaX/self.sliderWidth) * 100
+          transformPercentage = percentage + self.currentOffset
           if(percentage > -100 && percentage < 20) {
-            self.cards.style.transform = 'translateX(' + percentage + '%)';
+            self.cardsElem.style.transform = 'translateX(' + transformPercentage + '%)';
           }
           break;
         case 'panend':
-          var thing = (ev.deltaX/self.viewportSize*100)
-          if(thing < -50) {
+          var dragPercentage = (ev.deltaX/self.viewportSize*100)
+          if(dragPercentage < -50) {
             var newCard = self.activeCard + 1;
-            self.currentOffset =  -(100/self.slides.length * newCard - self.offsetPercent)
+            self.currentOffset =  -(100/self.cards.length * newCard - self.offsetPercent)
             goToSlide(self.activeCard + 1);
-          } else if(thing > 50) {
+          } else if(dragPercentage > 50) {
             var newCard = self.activeCard - 1;
-            self.currentOffset =  -( 100/self.slides.length * newCard - self.offsetPercent)
+            self.currentOffset =  -( 100/self.cards.length * newCard - self.offsetPercent)
             goToSlide(self.activeCard - 1);
           } else {
-            self.currentOffset =  - (100/self.slides.length * self.activeCard - self.offsetPercent)
+            self.currentOffset =  - (100/self.cards.length * self.activeCard - self.offsetPercent)
             goToSlide(self.activeCard);
           }
           break;
@@ -175,7 +179,7 @@ Slider.prototype = {
       mc.on('panleft panright panend', handleHammer)
     }
 
-    Array.prototype.map.call(this.cards.children, function(content) {
+    Array.prototype.map.call(this.cardsElem.children, function(content) {
        content = content.children[0]
        createHammer(content)
     })
@@ -189,23 +193,23 @@ module.exports = {
 const MUSTACHE_TEMPLATES = {
     "slider-cards-template":
       "<div class='slider-cards'>" +
-       "{{#slides}}" +
+       "{{#cards}}" +
        "<div class='slider-card {{class}}'>" +
          "<div class='slider-content'>" +
            "<h3>{{ title }}</h3>" +
            "<p>{{ text }}<p>" +
          "</div>" +
        "</div>" +
-       "{{/slides}}" +
+       "{{/cards}}" +
        "</div>",
     "nav-template":
       "<div class='nav'>" +
          "<ol>" +
-           "{{#slides}}" +
+           "{{#cards}}" +
              "<li class='nav nav-{{index}}'>" +
                "<a href='#'></a>" +
              "</li>" +
-           "{{/slides}}" +
+           "{{/cards}}" +
          "</ol>" +
        "</div>"
 }
