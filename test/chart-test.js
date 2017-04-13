10395bb067850828bd9d5007ef8dff596532131f
Divya Sasidharan <hello@shortdiv.com>
2017-04-13 17:03:12 -0500

    fix: nuking domain and range tests since we're using d3-axis to calculate this

--- test/chart-test.js
+++ test/chart-test.js
@@ -2,7 +2,7 @@ import { Chart } from'../src/js/chart.js';
 import { expect, assert } from 'chai'
 import sinon from 'sinon'
 import moment from 'moment'
-import d3Time from 'd3-time-format'
+const d3Time = require('d3-time-format');
 
 describe('ChartJS', () => {
   let Chart1;
@@ -11,87 +11,35 @@ describe('ChartJS', () => {
     beforeEach(() => {
       init = sinon.stub(Chart.prototype, 'createChart');
       let data = [
-                   [moment("01/21/80", "MM/DD/YY"), 1],
-                   [moment("02/29/80", "MM/DD/YY"), 3],
-                   [moment("03/31/80", "MM/DD/YY"), 2],
-                   [moment("04/30/80", "MM/DD/YY"), 3],
-                   [moment("05/30/80", "MM/DD/YY"), 2],
+                   [d3Time.timeParse("%m/%d/%Y")("01/21/80"), 1],
+                   [d3Time.timeParse("%m/%d/%Y")("02/29/80"), 3],
+                   [d3Time.timeParse("%m/%d/%Y")("03/31/80"), 2],
+                   [d3Time.timeParse("%m/%d/%Y")("04/30/80"), 3],
+                   [d3Time.timeParse("%m/%d/%Y")("05/30/80"), 2]
                  ];
       Chart1 = new Chart(
                  {
-                   //data: data,
-                   //bounds: {
-                   //  minX: d3Time.timeParse("%m/%d/%Y")("01/21/80"),
-                   //  maxX: d3Time.timeParse("%m/%d/%Y")("05/30/80"),
-                   //  minY: null,
-                   //  maxY: null
-                   //},
-                   //markers: null
+                   data: {},
+                   bounds: {
+                     minX: d3Time.timeParse("%m/%d/%Y")("01/21/80"),
+                     maxX: d3Time.timeParse("%m/%d/%Y")("05/30/80"),
+                     minY: null,
+                     maxY: null
+                   },
+                   markers: null
                  }, 500, 600)
     })
     it('should set a margin to default if given no input', () => {
       expect(Chart1.margin).to.eql({ 'top': 10, 'right': 30, 'bottom': 20, 'left': 30 })
     })
-    xit('should return the height of a new chart with margins accounted for', () => {
-      expect(Chart1.height).to.eql(540);
+    it('should return the height of a new chart with margins accounted for', () => {
+      expect(Chart1.height).to.eql(545);
     })
-    xit('should return the width of a new chart with margins accounted for', () => {
-      expect(Chart1.width).to.eql(430);
+    it('should return the width of a new chart with margins accounted for', () => {
+      expect(Chart1.width).to.eql(440);
     })
     afterEach(function() {
       init.restore();
-    })
-  })
-  describe('Create the domain', () => {
-    beforeEach(function() {
-      //range = sinon.stub(Chart1, 'setRange').returns(Chart1.rangeX = 1117494000000, Chart1.rangeY = 30)
-      Chart1.axes = {
-                      xAxis: null,
-                      xLabel: "month",
-                      xTick: 1,
-                    }
-    })
-    xit('should create an array of ticks with the start year as the first item and last year not included', () => {
-      var result = Chart1.createTicks(1980, 2015, 10)
-      expect(result.length).to.eq(4);
-      assert.deepEqual(result, [ 1980, 1990, 2000, 2010 ]);
-    })
-    xit('should create an array of ticks with the start year as first item and last year included', () => {
-      var result = Chart1.createTicks(1980, 2015, 5)
-      expect(result.length).to.eq(8);
-      assert.deepEqual(result, [ 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015 ]);
-    })
-    xit('should create an object of domain attributes', () => {
-      var result = Chart1.makeDomain()
-      expect(result.distance).to.eq(4);
-      assert.deepEqual(result.ticks, [0,1,2,3,4]);
-      expect(result.label).to.eq("month");
-    })
-    //afterEach(() =>
-      //range.restore()
-    //)
-  })
-  describe('Create the range', () => {
-    beforeEach(() => {
-      Chart1.axes = {
-                      yAxis: null,
-                      yLabel: "Number of Flights",
-                      yTick: 1,
-                    }
-      Chart1.bounds = {
-        minY: 1,
-        maxY: 5
-      }
-    })
-    xit('should create an array of ticks with start as first item in the list', () => {
-      var result = Chart1.createTicks(1,5)
-      expect(result.length).to.eq(5);
-      assert.deepEqual(result, [1,2,3,4,5]);
-    })
-    xit('should create an object of range attributes', () => {
-      const result = Chart1.makeRange();
-      assert.deepEqual(result.ticks, [1,2,3,4,5])
-      expect(result.label).to.eq("Number of Flights")
     })
   })
 })
