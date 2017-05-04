7e7baeb9cdab618402facdbe65c735885f89db33
Divya Sasidharan <hello@shortdiv.com>
2017-05-04 15:23:58 -0500

    fix: Extrapolating get request into a lib function

--- src/js/data.js
+++ src/js/data.js
@@ -114,21 +114,7 @@ DataFactory.prototype = {
    */
   get: function(file) {
     file = file ? file.data.url : undefined
-    return new Promise(function(resolve, reject) {
-      var req = new XMLHttpRequest();
-      req.open("GET", file, true)
-      req.onload = function() {
-        if(req.status == 200) {
-          resolve(req.response);
-        } else {
-          reject(Error(req.statusText));
-        }
-      }
-      req.onerror = function() {
-        reject(Error("Network Error"));
-      };
-      req.send();
-    });
+    return lib.get(file);
   },
 
   /**
--- src/js/lib.js
+++ src/js/lib.js
@@ -28,9 +28,28 @@ var lib = (function() {
     storyline.elem.append(doc.body.children[0])
   }
 
+  function get(url) {
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open("GET", url, true);
+      req.onload = function() {
+        if(req.status == 200) {
+          resolve(req.response)
+        } else {
+          reject(Error(req.statusText))
+        }
+      }
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      }
+      req.send();
+    })
+  }
+
   return {
     onResize: onResize,
     errorLog: errorLog
+    get: get
   }
 })();
 
