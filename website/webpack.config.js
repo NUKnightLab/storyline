1f693e1178b5cdd159fa2420193456a49bf5cb44
Joe Germuska <joe@germuska.com>
2017-08-11 10:40:10 -0500

    first pass at an npm/webpack website dev with some of the authoring inlined

---
+++ website/webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack'),
+    path = require('path'),
+
+    componentPath = path.resolve('./src/js');
+
+module.exports = {
+  context: path.join(__dirname),
+  entry: {
+    website: "./src/js/website.js"
+  },
+  output: {
+    path: path.join(__dirname, "./dist/js"),
+    filename: "[name].js",
+    libraryTarget: 'var',
+    library: "[name]"
+  },
+  node: {
+    fs: "empty",
+    "child_process": "empty"
+  },
+  "browser": { "fs": false },
+  resolve: {
+    root: componentPath
+  },
+  resolveLoader: {
+    root: path.join(__dirname, "node_modules")
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  webpackMiddleware: {
+    noInfo: true
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      PubSub: "pubsub-js"
+    })
+  ]
+}
