c777213b5fb08b461b137ba4d46d7917cf253e0b
Joe Germuska <joe@germuska.com>
2017-08-18 16:00:41 -0500

    implement handlebars templates so that we can load the JS/CSS from different servers depending upon whether running locally, or on a staging or production server

---
+++ website/tasks/compile-hbs.js
@@ -0,0 +1,49 @@
+var _ = require('lodash'),
+    yml = require('yamljs'),
+    fs = require('fs-extra'),
+    path = require('path'),
+    Handlebars = require('handlebars'),
+    globby = require('globby');
+
+/**
+ * renderPage: passes a specific template through a layout and with associated data through a hbs function and returns a string
+ *
+ * @param string template
+ * @param string layout
+ * @param object data
+ * @returns string html
+ */
+function renderPage(template, context) {
+  var file = fs.readFileSync(template, 'utf8'),
+      page = Handlebars.compile(file);
+  return page(context);
+}
+
+/**
+ * build: compile all templates under src/templates and output them to equivalent paths in dist
+ *
+ * @returns {undefined}
+ */
+function build() {
+  var hbsviews = globby.sync('src/templates/**/*.hbs');
+  var config_context = process.argv[2] || 'dev';
+  var context = yml.load(`src/data/${config_context}.yml`);
+  _.forEach(hbsviews, function(file, i) {
+    var filePattern = path.dirname(file).split('src/templates/')[1],
+        fileName = path.basename(file, '.hbs'),
+        outputPath = null;
+
+    if(filePattern === undefined) {
+      outputPath = `dist/${fileName}.html`;
+    } else {
+      outputPath = `dist/${filePattern}/${fileName}.html`;
+    }
+
+    var page = renderPage(file, context);
+
+    fs.outputFileSync(outputPath, page, 'utf8')
+
+  });
+}
+
+build();
