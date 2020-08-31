34aecc24d926f7ff035ec62588e40fc26f40014b
Joe Germuska <joe@germuska.com>
2020-08-31 15:35:04 -0500

    things seem to be working and fewer warnings

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -4,45 +4,19 @@ var webpack = require('webpack'),
     componentPath = path.resolve('./src/js');
 
 module.exports = {
-  context: path.join(__dirname),
-  entry: {
-    website: "./src/js/website.js"
-  },
-  output: {
-    path: path.join(__dirname, "./dist/js"),
-    filename: "[name].js",
-    libraryTarget: 'var',
-    library: "[name]"
-  },
-  node: {
-    fs: "empty",
-    "child_process": "empty"
-  },
-  "browser": { "fs": false },
-  resolve: {
-    root: componentPath
-  },
-  resolveLoader: {
-    root: path.join(__dirname, "node_modules")
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  webpackMiddleware: {
-    noInfo: true
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      PubSub: "pubsub-js"
-    })
-  ]
+    context: path.join(__dirname),
+    entry: {
+        website: "./src/js/website.js"
+    },
+    output: {
+        path: path.join(__dirname, "./dist/js"),
+        filename: "[name].js",
+        libraryTarget: 'var',
+        library: "[name]"
+    },
+    node: {
+        fs: "empty",
+        "child_process": "empty"
+    },
+    target: 'web'
 }
